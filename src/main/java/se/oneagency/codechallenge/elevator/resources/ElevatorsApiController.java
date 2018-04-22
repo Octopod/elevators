c797c571428a9bc7b2842f1626429e7bf1537f18
Octopod <denise.nordlof@gmail.com>
2018-04-22 17:36:23 +0200

    first test of ElevatorsApiController implemented and passed

--- src/main/java/se/oneagency/codechallenge/elevator/resources/ElevatorsApiController.java
+++ src/main/java/se/oneagency/codechallenge/elevator/resources/ElevatorsApiController.java
@@ -1,5 +1,9 @@
 package se.oneagency.codechallenge.elevator.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import se.oneagency.codechallenge.elevator.api.ElevatorsApi;
 
@@ -8,6 +12,16 @@ import se.oneagency.codechallenge.elevator.api.ElevatorsApi;
  *
  */
 @RestController
-public final class ElevatorsApiController implements ElevatorsApi {
+public final class ElevatorsApiController implements ElevatorsApi{
+    Logger log = LoggerFactory.getLogger(ElevatorsApiController.class);
+
+
+    @Override
+    public ResponseEntity<Void> elevators() {
+        log.debug("# ElevatorAPI PING #");
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+
 
 }
