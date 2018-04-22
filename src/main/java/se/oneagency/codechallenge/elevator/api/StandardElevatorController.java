03485c2a3e58b495469dca04bc68c4a1d982cf57
Octopod <denise.nordlof@gmail.com>
2018-04-22 17:19:51 +0200

    first draft StandardElevatorController

--- src/main/java/se/oneagency/codechallenge/elevator/api/StandardElevatorController.java
+++ src/main/java/se/oneagency/codechallenge/elevator/api/StandardElevatorController.java
@@ -1,20 +1,44 @@
 package se.oneagency.codechallenge.elevator.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class StandardElevatorController implements ElevatorController {
+class StandardElevatorController implements ElevatorController {
+    private final List<StandardElevator> listOfElevators = new ArrayList<>();
+    private int nrOfFloors;
+    private int nrOfElevators;
+
+    public StandardElevatorController(int nrOfFloors, int nrOfElevators){
+        this.nrOfFloors = nrOfFloors;
+        this.nrOfElevators = nrOfElevators;
+
+        for (int i = 0; i < nrOfElevators; i++) {
+            listOfElevators.add(new StandardElevator(i,0));
+        }
+    }
+
+    /**
+     * Placeholder.
+     * returns the first non-busy elevator*/
     @Override
-    public Elevator requestElevator(int toFloor) {
+    public StandardElevator requestElevator(int toFloor) {
+        List<StandardElevator> possibleElevators = getElevators();
+        for (StandardElevator e : possibleElevators) {
+            if(!e.isBusy()){
+                return e;
+            }
+        }
         return null;
     }
 
     @Override
-    public List<Elevator> getElevators() {
-        return null;
+    public List<StandardElevator> getElevators() {
+        return this.listOfElevators;
     }
 
     @Override
-    public void releaseElevator(Elevator elevator) {
+    public void releaseElevator(StandardElevator elevator) {
+        elevator.moveElevator(elevator.currentFloor());
 
     }
 }
